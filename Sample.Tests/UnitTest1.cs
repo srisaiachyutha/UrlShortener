6bc5e9c42f8cef4b91371e18cc46e9e834139747
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2023-07-28 06:41:34 +0000

    ddclear

---
+++ Sample.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace Sample.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+}
