03319bd7968481a3ef4af8fe11003f229b4b9e5b
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2023-09-09 12:40:05 +0000

    updated the connection string names

--- UrlShortner/ApplicationSettings.cs
+++ UrlShortner/ApplicationSettings.cs
@@ -2,8 +2,8 @@ namespace UrlShortner
 {
     public class ApplicationSettings
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserId { get; set; }
+        public string SecurePassword { get; set; }
         public string SecureConnectionBundlePath { get; set; }
     }
 }
